ef92179cec2393b1f93f06feb1c97475714fb0a7
Majid Bhuiyan <majid.bhuiyan2021@gmail.com>
2026-02-27 14:05:14 +0600

    feat: init firebase Crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdigi/app/app.dart';
 
 import 'firebase_options.dart';
 
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(const RxDigi());
-}
+void main() async {
+  runZonedGuarded<Future<void>>(() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    await Firebase.initializeApp();
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 
+    runApp(RxDigi());
+  }, (error, stack) => FirebaseCrashlytics.instance.recordError(error, stack));
+}
